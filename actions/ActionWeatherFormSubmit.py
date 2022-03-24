1444b84f7d92d2e4f64fa9e09db4f334dcb107be
billynguyenlss <billynguyen.lss@gmail.com>
2022-03-24 11:48:21 +0700

    2nd mvp

---
+++ actions/ActionWeatherFormSubmit.py
@@ -0,0 +1,34 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from datetime import date, timedelta
+
+
+class ActionWeatherFormSubmit(Action):
+
+    def name(self) -> Text:
+        return "action_weather_form_submit"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        city = tracker.get_slot("address")
+        text_date = tracker.get_slot("date")
+        # query_date = self._get_date(text_date)
+
+        msg = f"Thời tiết hôm nay tại {city} là {text_date}"
+        dispatcher.utter_message(msg)
+
+        return []
+
+    def _get_date(self, text_date):
+        if text_date in ["hôm nay", "hom nay"]:
+            return date.today()
+        if text_date in ["ngày mai", "ngay mai", "tomorrow"]:
+            tomorrow = date.today() + timedelta(days=1)
+            return tomorrow
+        if text_date in ["hôm qua", "hom qua", "yesterday"]:
+            yesterday = date.today() - timedelta(days=1)
+            return yesterday
+        return date.today()
--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,48 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from datetime import datetime, date, timedelta
+
+class ActionQueryTime(Action):
+
+    def name(self) -> Text:
+        return "action_query_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        current_time = datetime.now().strftime("It's %H:%M:%S")
+
+        dispatcher.utter_message(text=current_time)
+
+        return []
+
+class ActionQueryDate(Action):
+
+    def name(self) -> Text:
+        return "action_query_date"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        text_date = tracker.get_slot("date")
+
+        int_date = text_date_to_int(text_date)
+        dispatcher.utter_message(text=int_date)
+
+        return []
+
+def text_date_to_int(text_date):
+    if text_date in ["hôm nay","hom nay"]:
+        return date.today().strftime('%Y-%m-%d')
+    if text_date in ["ngày mai", "ngay mai", "tomorrow"]:
+        tomorrow = date.today() + timedelta(days=1)
+        return tomorrow.strftime('%Y-%m-%d')
+    if text_date in ["hôm qua","hom qua","yesterday"]:
+        yesterday =  date.today() - timedelta(days=1)
+        return yesterday.strftime('%Y-%m-%d')
+    return []
